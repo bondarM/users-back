8ee928a1061b6fe2bea5bcdb5ef03f817ec6002a
bondarM <mihailbondar94@gmail.com>
2023-04-07 17:05:37 +0300

    cors added

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ import express from 'express'
 import dotenv from 'dotenv'
 import authRouter from './routes/authRouter.js'
 import errorMiddleware from './middleware/errorMiddleware.js'
-
+import cors from 'cors'
 dotenv.config()
 
 const PORT = process.env.PORT || 5001
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use("/api", authRouter)
 app.use(errorMiddleware)
